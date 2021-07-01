4d7342542e2208a1a78c9e7b7f2c7e96406a5d57
Dmitrii Rogoznyi <k911mipt@gmail.com>
2021-07-02 02:35:26 +0300

    Fix error with creatives picking being unequiprobable

--- src/auction.py
+++ src/auction.py
@@ -1,9 +1,9 @@
-from typing import Dict, Generator, List, Optional, Tuple
+import dataclasses
+import numpy as np
+
+from typing import Dict, Generator, List, Optional
 
 import src.creative as creative
-
-import operator
-import random
 
 
 Creative = creative.Creative
@@ -12,6 +12,8 @@ IdType = creative.IdType
 CountryType = creative.CountryType
 IdCreativesMapType = Dict[IdType, List[Creative]]
 PriceIdMapType = Dict[PriceType, List[IdType]]
+
+RNG = np.random.default_rng(42)
 
 
 def get_creatives_filtered_by_country(
@@ -40,88 +42,84 @@ def get_unique_advertises_with_lowest_price(
                 unique_creatives[entity.advertiser_id] = [entity]
             elif entity.price == existing_entity.price:
                 existing_entities.append(entity)
+
     return unique_creatives
 
 
 def get_price_id_map(unique_creatives: IdCreativesMapType) -> PriceIdMapType:
     price_id_map: PriceIdMapType = {}
     for id, entity in unique_creatives.items():
-        list_of_ids = price_id_map.get(entity[0].price)
-        if list_of_ids is not None:
-            list_of_ids.append(id)
+        list_ids = price_id_map.get(entity[0].price)
+        if list_ids is not None:
+            list_ids.append(id)
         else:
             price_id_map[entity[0].price] = [id]
+
     return price_id_map
 
 
-def get_candidates(
-    price_id_map: PriceIdMapType,
-) -> List[tuple[PriceType, List[IdType]]]:
+@dataclasses.dataclass
+class Candidate:
+    price: PriceType
+    ids: List[IdType]
+
+
+def get_candidates(price_id_map: PriceIdMapType,) -> List[Candidate]:
     return sorted(
-        ((price, list_of_ids) for price, list_of_ids in price_id_map.items()),
-        key=operator.itemgetter(0),
+        (Candidate(price, ids) for price, ids in price_id_map.items()),
+        key=lambda candidate: candidate.price,
     )
 
 
-def get_weighted_candidates(
-    candidates: List[tuple[PriceType, List[IdType]]],
+def get_probabilities(list_ids, unique_creatives):
+    # Calculate probabilities using number of id met in distribution
+    total_weight = sum([len(unique_creatives[id]) for id in list_ids])
+    probabilities = [len(unique_creatives[id]) / total_weight for id in list_ids]
+
+    return probabilities
+
+
+def pick_next_winners(
+    candidate: Candidate,
+    number_to_pick: int,
     unique_creatives: IdCreativesMapType,
-) -> List[dict]:
-    weighted_candidates = []
-    for price, list_of_ids in candidates:
-        accumulated_weight = 0
-        list_weighted_ids = []
-        for id in list_of_ids:
-            weight = len(unique_creatives[id])
-            accumulated_weight += weight
-            list_weighted_ids.append(
-                {"id": id, "win_treshold": accumulated_weight}
-            )
-        weighted_candidates.append(
-            {
-                "price": price,
-                "total_weight": accumulated_weight,
-                "list_weighted_ids": list_weighted_ids,
-            }
-        )
-    return weighted_candidates
-
-
-def pick_next_winner(weighted_candidates, price_index):
-    item = weighted_candidates[price_index]
-    list_weighted_ids = item["list_weighted_ids"]
-    if len(list_weighted_ids) == 1:
-        price_index += 1
-        winner_id = list_weighted_ids[0]["id"]
+    rng: np.random.Generator,
+):
+    list_ids = candidate.ids
+    winner_ids: list[IdType]
+    if len(list_ids) <= number_to_pick:
+        winner_ids = list_ids
     else:
-        total_weight = list_weighted_ids[-1]["win_treshold"]
-        winner_weight = random.randint(0, total_weight - 1)
-        winner_item = None
-        for item in list_weighted_ids:
-            if item["win_treshold"] > winner_weight:
-                winner_item = item
-                break
-        assert winner_item is not None
-        winner_id = winner_item["id"]
-        list_weighted_ids.remove(winner_item)
-    return winner_id, price_index
+        # Pick ids equiprobable
+        probabilities = get_probabilities(list_ids, unique_creatives)
+        winner_ids = rng.choice(list_ids, number_to_pick, p=probabilities)
+
+    return winner_ids
 
 
-def get_winner_entities(weighted_candidates, unique_creatives, number_winners):
-    price_index = 0
+def get_winner_entities(
+    candidates: List[Candidate],
+    unique_creatives: IdCreativesMapType,
+    number_winners: int,
+    rng: np.random.Generator,
+):
     number_won = 0
     winner_entities = []
-    while number_won < number_winners and price_index < len(weighted_candidates):
-        winner_id, price_index = pick_next_winner(
-            weighted_candidates, price_index
+    for candidate in candidates:
+        winner_ids = pick_next_winners(
+            candidate, number_winners - number_won, unique_creatives, rng
         )
-        list_winner_creatives = unique_creatives[winner_id]
-        len_creatives = len(list_winner_creatives)
-        winner_index = (
-            random.randint(0, len_creatives - 1) if len_creatives > 1 else 0
-        )
-        winner_entities.append(list_winner_creatives[winner_index])
-        number_won += 1
+        for winner_id in winner_ids:
+            list_winner_creatives = unique_creatives[winner_id]
+            len_creatives = len(list_winner_creatives)
+            assert len_creatives > 0
+            # Each creative has an equal probability within same id group
+            winner_index = rng.integers(0, len_creatives)
+            winner_entities.append(list_winner_creatives[winner_index])
+        number_won += len(winner_ids)
+        if number_won >= number_winners:
+            break
+
     return winner_entities
 
 
@@ -129,21 +127,19 @@ def auction(
     creatives: List[Creative],
     number_winners: int,
     country: Optional[CountryType] = None,
-    seed: Optional[int] = None,
+    rng: Optional[np.random.Generator] = None,
 ):
-    if seed is not None:
-        random.seed(seed)
+    if rng is None:
+        rng = RNG
     # Build a dict of unique advertiser creatives with lowest price
     unique_creatives = get_unique_advertises_with_lowest_price(creatives, country)
     # Build a dict of unique prices with id lists
     price_id_map = get_price_id_map(unique_creatives)
     # Build a list of winner candidates
     candidates = get_candidates(price_id_map)
-    # Calculate weights of multiple ids with same price
-    weighted_candidates = get_weighted_candidates(candidates, unique_creatives)
     # Build final winner list
     winner_entities = get_winner_entities(
-        weighted_candidates, unique_creatives, number_winners
+        candidates, unique_creatives, number_winners, rng
     )
 
     return winner_entities
